1397d024ad31878f23bcaeb6ffe2ec94a0f2e0fc
Eliott <eliott.chauchard@orange.fr>
2020-09-22 16:47:22 +0200

    Le joueur qui bouge

---
+++ RogueLike/Assets/Scripts/ELC_PlayerMoves.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ELC_PlayerMoves : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    public bool canMove;
+
+    private Vector3 playerMoves;
+
+    void Update()
+    {
+        if (canMove)
+        {
+            Moves();
+        }
+
+        Raycasts();
+    }
+
+    void Moves()
+    {
+        playerMoves.x = Input.GetAxis("Horizontal") * speed;
+        playerMoves.y = Input.GetAxis("Vertical") * speed;
+        playerMoves = Vector3.ClampMagnitude(playerMoves, speed);
+        transform.Translate(playerMoves * Time.deltaTime);
+    }
+
+    void Raycasts()
+    {
+        Debug.DrawRay(this.transform.position, transform.TransformDirection(playerMoves), Color.green);
+    }
+}
