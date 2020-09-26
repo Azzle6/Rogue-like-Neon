f92c0f8ec8e691e7ca22205c273f48dbd6135a4a
Alexandre Daspe <alexandre.daspe@gmail.com>
2020-09-26 18:33:19 +0200

    Gash almost working

---
+++ RogueLike/Assets/Scripts/AXD_Scripts/AXD_PlayerAttack.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AXD_PlayerAttack : MonoBehaviour
+{
+
+    private enum AttackType { Gash, Thrust}
+    public Transform attackPoint;
+    public ELC_PlayerMoves player;
+    public int Damage;
+    [Header ("Attack Settings")]
+    [Range (0,5)]
+    public float areaRadius;
+    public float attackRate;
+    public float nextAttackTime;
+
+    void Start()
+    {
+        nextAttackTime = Time.time;
+    }
+    void Update()
+    {
+        attackPoint.position = (transform.position + player.getPlayerMoves().normalized);
+        Debug.Log("Attaquer ? "+(Time.time >= nextAttackTime));
+        if (Time.time >= nextAttackTime)
+        {
+            if (Input.GetAxisRaw("Gash") != 0)
+            {
+                Debug.Log("Gash : " + player.getPlayerMoves().ToString());
+                Attack(AttackType.Gash);
+            }
+            else if (Input.GetAxisRaw("Thrust") != 0)
+            {
+                Debug.Log("Thrust : " + player.getPlayerMoves().ToString());
+                this.Attack(AttackType.Thrust);
+            }
+            
+        }
+    }
+
+    private void Attack(AttackType type)
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position,areaRadius);
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<AXD_Enemy>().GetHit(Damage);
+        }
+        nextAttackTime = Time.time + 1f / attackRate;
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint != null)
+            Gizmos.DrawWireSphere(attackPoint.position, areaRadius);
+    }
+}
--- RogueLike/Assets/Scripts/ELC_RandomObjectGenerator.cs
+++ RogueLike/Assets/Scripts/ELC_RandomObjectGenerator.cs
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -42,48 +42,4 @@ public class ELC_RandomObjectGenerator : MonoBehaviour
 
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class ELC_RandomObjectGenerator : MonoBehaviour
-{
-    public List<GameObject> objectsList = new List<GameObject>();
-    public List<float> spawnChance = new List<float>();
-    
-    
-
-    void Start()
-    {
-        float totalChances = 0;
-        for (int i = 0; i < spawnChance.Count; i++)
-        {
-            totalChances += spawnChance[i];
-        }
-
-        for (int i = 0; i < spawnChance.Count; i++)
-        {
-            spawnChance[i] /= totalChances;
-        }
-
-        float randomNumber = Random.value;
-        Debug.Log(randomNumber);
-
-        float count = 0;
-        for (int i = 0; i < spawnChance.Count; i++)
-        {
-            count += spawnChance[i];
-            Debug.Log(count);
-
-            if (count >= randomNumber)
-            {
-                Object.Instantiate(objectsList[i], this.transform.position, Quaternion.identity);
-                Debug.Log(objectsList[i] + " has been instantiate.");
-                break;
-            }
-        }
-
-    }
-}
->>>>>>> Stashed changes
