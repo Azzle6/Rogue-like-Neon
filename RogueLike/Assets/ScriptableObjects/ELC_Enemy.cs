5634b8952740bade1eb6bd8745f96dabcf21da0e
Eliott <eliott.chauchard@orange.fr>
2020-09-28 14:19:56 +0200

    Changements sur certains Scriptable objects

---
+++ RogueLike/Assets/ScriptableObjects/ELC_Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enemy", menuName = "ScriptableObjects/EnemyScriptableObject", order = 1)]
+public class ELC_Enemy : ScriptableObject
+{
+    public string enemyName;
+    public float maxHeal;
+    public float strenght;
+    public float speed;
+    public float moneyToDropWhenHit;
+    public float moneyToDropWhenDead;
+
+    public bool followPlayer;
+    public bool fleePlayer;
+    public bool stayAtDistance;
+    public float distanceToStay;
+
+
+    public bool fireBullets;
+    public GameObject bullet;
+
+    public bool instantiateSomethingAtDeath;
+    public GameObject thingToInstanciate;
+
+    public bool dashOnPlayer;
+    public float dashSpeed;
+    public float dashTime;
+    public float attackCooldown;
+
+
+    public Sprite sprite;
+    public MonoBehaviour AIScript;
+    public Animator Animator;
+}
---
+++ RogueLike/Assets/ScriptableObjects/ELC_PowerUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="PowerUp", menuName = "ScriptableObjects/PowerUpScriptableObject", order = 1)]
+public class ELC_PowerUp : ScriptableObject
+{
+    //Pour le type de Power Up
+    public enum Type {Attack, Speed, Heal};
+    public Type type;
+
+    //Pour son niveau de puissance
+    public int level;
+
+    //De compbien il multiplie la valeur
+    [Range(1, 4)]
+    public float multiplicator;
+
+    //Durée du PowerUp
+    public float time;
+}
